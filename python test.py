9a10b09fa09ade2ae7239f745c42f869a1af3ab2
Stylianos Thomas <stylianos.thomas@gmail.com>
2019-09-18 19:46:30 +0300

    Testing some simple operations

--- python test.py
+++ python test.py
@@ -1,4 +1,12 @@
-a = "This is "
-b = "Sparta"
-c = a + b 
-print (c)
+# Testing some simple operations
+strA = "This is program that helps you calculate your age. "
+strB = "All you need to do is provide some information."
+
+print(strA + strB)
+strC = str(input("Please insert your name: "))
+
+intA = 2019
+intB = int(input("Please insert the year were born: "))
+intC = intA - intB
+
+print(strC, "you are", intC, "years old.")
